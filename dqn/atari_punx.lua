c0b7a417f1005faaf9b1ce36c47795aff979318d
Islam Jambei <islam@e-magic.org>
2015-07-24 07:51:42 +0300

    init commit

---
+++ dqn/atari_punx.lua
@@ -0,0 +1,18 @@
+--[[
+Copyright (c) 2014 Google Inc.
+
+See LICENSE file for full terms of limited license.
+]]
+
+require 'punxnet'
+
+return function(args)
+    args.n_units        = {24, 32, 32}
+    args.filter_size    = {5, 3, 2}
+    args.filter_stride  = {4, 3, 2}
+    args.n_hid          = {256, 256}
+    args.nl             = nn.PReLU
+
+    return create_network(args)
+end
+
